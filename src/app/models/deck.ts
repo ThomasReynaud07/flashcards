9109116d1c9e5fde8c39f259ad737379fb67a978
Thomas <pk84vtk@eduvaud.ch>
2026-02-11 13:14:15 +0100

    feat(migration&models):Creation migration et models
    [52][Done]
    J'ai créer les 2 tables (decks et flashcards) et je les aient migrés sur ma db. Cela m'a pris du temps car j'ai eu un problème de repository au tt début de la période. J'ai finalement réussi a pull les modifications.

---
+++ src/app/models/deck.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Flashcard from '#models/flashcard'
+
+export default class Deck extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @hasMany(() => Flashcard)
+  declare flashcards: HasMany<typeof Flashcard>
+}
---
+++ src/app/models/flashcard.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Deck from '#models/deck'
+
+export default class Flashcard extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => Deck)
+  declare deck: BelongsTo<typeof Deck>
+}
