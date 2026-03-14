0f60ab3737d68b7e9d583c205f964517d0ddc4a1
Thomas <pk84vtk@eduvaud.ch>
2026-03-14 10:44:40 +0100

    update(seader):Ajout de plus de decks au début
    [20][Done]
    J'ai ajouté plus de cartes au seeders afin d'avoir quelques decks, cétait surtout pour mes tests de l'analyse fonctionnelle.

--- src/database/seeders/flashcard_seeder.ts
+++ src/database/seeders/flashcard_seeder.ts
@@ -5,20 +5,90 @@ import Flashcard from '#models/flashcard'
 export default class extends BaseSeeder {
   async run() {
     const deckCapitales = await Deck.create({
-      title: 'Capitales',
-      description: 'Apprendre les capitales',
+      title: 'Capitales du Monde',
+      description: 'Testez vos connaissances sur les capitales internationales.',
     })
 
     await Flashcard.createMany([
       {
-        question: 'Capital de allemagne ?',
+        question: "Quelle est la capitale de l'Allemagne ?",
         answer: 'Berlin',
         deckId: deckCapitales.id,
       },
       {
-        question: 'Capital de Suisse ?',
+        question: 'Quelle est la capitale de la Suisse ?',
         answer: 'Berne',
         deckId: deckCapitales.id,
+      },
+      {
+        question: 'Quelle est la capitale du Japon ?',
+        answer: 'Tokyo',
+      },
+      {
+        question: 'Quelle est la capitale du Canada ?',
+        answer: 'Ottawa',
+        deckId: deckCapitales.id,
+      },
+      {
+        question: "Quelle est la capitale de l'Australie ?",
+        answer: 'Canberra',
+        deckId: deckCapitales.id,
+      },
+    ])
+
+    const deckJS = await Deck.create({
+      title: 'JavaScript Avancé',
+      description: 'Concepts clés du langage JS et Node.js.',
+    })
+
+    await Flashcard.createMany([
+      {
+        question: 'Que signifie "NaN" ?',
+        answer: 'Not a Number',
+        deckId: deckJS.id,
+      },
+      {
+        question: 'Quelle méthode transforme un objet JSON en chaîne de caractères ?',
+        answer: 'JSON.stringify()',
+        deckId: deckJS.id,
+      },
+      {
+        question: 'Quelle est la différence entre "let" et "var" ?',
+        answer: 'let a une portée de bloc, var a une portée de fonction.',
+        deckId: deckJS.id,
+      },
+      {
+        question: "Comment appelle-t-on une fonction qui s'appelle elle-même ?",
+        answer: 'Une fonction récursive',
+        deckId: deckJS.id,
+      },
+    ])
+
+    const deckEnglish = await Deck.create({
+      title: 'Anglais - Business',
+      description: 'Vocabulaire utile pour le monde du travail.',
+    })
+
+    await Flashcard.createMany([
+      {
+        question: 'Comment dit-on "Compte rendu" en anglais ?',
+        answer: 'Minutes (of the meeting)',
+        deckId: deckEnglish.id,
+      },
+      {
+        question: 'Traduisez : "To schedule a meeting"',
+        answer: 'Planifier une réunion',
+        deckId: deckEnglish.id,
+      },
+      {
+        question: 'Que signifie l\'idiome "To call it a day" ?',
+        answer: "S'arrêter pour aujourd'hui (finir sa journée)",
+        deckId: deckEnglish.id,
+      },
+      {
+        question: 'Comment dit-on "Congés payés" ?',
+        answer: 'Paid leave / Paid holidays',
+        deckId: deckEnglish.id,
       },
     ])
   }
